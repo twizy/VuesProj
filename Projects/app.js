be2814078ae983d9c95f4e3a44fcf3a8989c4756
twizy <twizalain@gmail.com>
2020-09-17 23:16:54 +0200

    New version

--- Projects/app.js
+++ Projects/app.js
@@ -6,19 +6,89 @@ new Vue({
         playerHealth: 100,
         monsterHealth: 100,
         gameIsRunning: false,
+        turns: [],
+        pl:"",mo:"",
     },
     methods: {
+
         startGame: function () {
             this.gameIsRunning = true;
             this.playerHealth = 100;
             this.monsterHealth = 100;
         },
-        attack: function () {
-            var damage = this.calculateDamage(3, 10);
+
+        launchGame: function () {
+            this.gameIsRunning = false;
+            this.playerHealth = 100;
+            this.monsterHealth = 100;
+            this.turns = [];
+        },
+
+        myAttack: function () {
+            var damage = Math.max(Math.floor(Math.random() * 10), 3);
             this.monsterHealth -= damage;
+            this.monsterAttack()
+
+            this.mo = "Monster punched " + damage + " punch(es)";
+            this.turns.unshift({
+                text: this.mo,
+            });
+
+            if (this.monsterHealth <= 5) {
+                this.monsterHealth = 0;
+                alert("Continue");
+                this.startGame();
+                this.turns = [];
+            }
         },
-        calculateDamage: function(min, max) {
-            return Math.max(Math.floor(Math.random() * max) + 1, min);
+
+        monsterAttack: function () {
+            var damage = Math.max(Math.floor(Math.random() * 10), 3);
+            this.playerHealth -= damage;
+
+            this.pl = "Player punched " + damage + " punch(es)";
+            this.turns.unshift({
+                text: this.pl,
+            });
+        
+            if (this.playerHealth <= 5) {
+                this.playerHealth = 0;
+                alert("Game over");
+                this.launchGame();
+            }
         },
+
+        specialAttack: function () {
+            var damage = Math.max(Math.floor(Math.random() * 13), 5);
+            this.monsterHealth -= damage;
+
+            this.mo = "Monster punched " + damage + " punch(es)";
+            this.turns.unshift({
+                text: this.mo,
+            });
+
+            this.monsterAttack();
+ 
+            if (this.monsterHealth <= 5) {
+                this.monsterHealth = 0;
+                alert("Continue")
+                this.turns = [];
+                this.startGame();
+            }
+              
+        },
+
+        heal: function () {
+            var damage = Math.max(Math.floor(Math.random() * 10), 3);
+            this.playerHealth += damage;
+            if (this.playerHealth >= 90) {
+                this.playerHealth -= 10;
+            }
+        },
+
+        giveUp: function () {
+            this.launchGame()
+        },
+
     }
 });
